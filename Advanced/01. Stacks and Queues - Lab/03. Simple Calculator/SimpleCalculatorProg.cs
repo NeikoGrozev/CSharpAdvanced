20d5e4dc9ff04f010d41cf4a7f34f5155f4e59f3
Neiko7 <neiko_grozev@abv.bg>
2019-07-30 21:19:01 +0300

    New task...

---
+++ Advanced/01. Stacks and Queues - Lab/03. Simple Calculator/SimpleCalculatorProg.cs
@@ -0,0 +1,35 @@
+﻿namespace SimpleCalculator
+{
+    using System;
+        using System.Collections.Generic;
+    using System.Linq;
+
+    class SimpleCalculatorProg
+    {
+        static void Main()
+        {
+            string[] array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Stack<string> stack = new Stack<string>(array.Reverse());
+            
+            while (stack.Count != 1)
+            {
+                int firstDigit = int.Parse(stack.Pop());
+                string symbol = stack.Pop();
+                int secondDigit = int.Parse(stack.Pop());
+
+                if (symbol == "+")
+                {
+                    stack.Push((firstDigit + secondDigit).ToString());
+                }
+                else if (symbol == "-")
+                {
+                    stack.Push((firstDigit - secondDigit).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());                
+        }
+    }
+}
