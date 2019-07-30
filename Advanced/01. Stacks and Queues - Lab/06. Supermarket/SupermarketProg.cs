6e300aa0ffa34b0f09ee50875f1e9c2bddb60c3a
Neiko7 <neiko_grozev@abv.bg>
2019-07-30 22:09:30 +0300

    New task...

---
+++ Advanced/01. Stacks and Queues - Lab/06. Supermarket/SupermarketProg.cs
@@ -0,0 +1,45 @@
+﻿namespace Supermarket
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    class SupermarketProg
+    {
+        static void Main()
+        {
+            Queue<string> queuePaid = new Queue<string>();
+            Queue<string> queue = new Queue<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+                else if (input == "Paid")
+                {
+                    int count = queue.Count;
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        string name = queue.Dequeue();
+                        queuePaid.Enqueue(name);
+                    }
+
+                    continue;
+                }
+
+                queue.Enqueue(input);
+            }
+            if (queuePaid.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, queuePaid));
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
