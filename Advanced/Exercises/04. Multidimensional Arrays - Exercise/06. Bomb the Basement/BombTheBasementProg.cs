f5acd39bae9b2b5b7f771ffb1b40c7f90c75dc65
Neiko7 <neiko_grozev@abv.bg>
2019-08-12 11:03:20 +0300

    New task...

---
+++ Advanced/Exercises/04. Multidimensional Arrays - Exercise/06. Bomb the Basement/BombTheBasementProg.cs
@@ -0,0 +1,72 @@
+﻿namespace BombTheBasement
+{
+    using System;
+    using System.Linq;
+
+    class BombTheBasementProg
+    {
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] parameters = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rowsMatrix = dimensions[0];
+            int colsMatrix = dimensions[1];
+            int[,] matrix = new int[rowsMatrix, colsMatrix];
+
+            int currentRowBomb = parameters[0];
+            int currentColBomb = parameters[1];
+            int radius = parameters[2];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    bool isInRadius = Math.Pow(row - currentRowBomb, 2) + Math.Pow(col - currentColBomb, 2) <=
+                        Math.Pow(radius, 2);
+
+                    if (isInRadius)
+                    {
+                        matrix[row, col] = 1;
+                    }
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int counter = 0;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    if (matrix[row, col] == 1)
+                    {
+                        counter++;
+                        matrix[row, col] = 0;
+                    }
+                }
+
+                for (int row = 0; row < counter; row++)
+                {
+                    matrix[row, col] = 1;
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Advanced/Exercises/04. Multidimensional Arrays - Exercise/07. Knight Game/KnightGameProg.cs
@@ -0,0 +1,93 @@
+﻿namespace KnightGame
+{
+    using System;
+    using System.Linq;
+
+    class KnightGameProg
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[rows, rows];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] newLine = Console.ReadLine()
+                    .ToCharArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = newLine[col];
+                }
+            }
+
+            int[] LPattern = new int[] {
+                 1,  2,
+                 1, -2,
+                 2,  1,
+                 2, -1,
+                -1,  2,
+                -1, -2,
+                -2,  1,
+                -2, -1,
+            };
+
+            int count = 0;
+
+            while (true)
+            {
+                int knightRow = -1;
+                int knightCol = -1;
+                int maxKnight = 0;
+
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        int currentKnight = 0;
+
+                        if (matrix[row, col] == 'K')
+                        {
+                            for (int i = 0; i < LPattern.Length; i += 2)
+                            {
+                                if (IsInside(matrix, row + LPattern[i], col + LPattern[i + 1])
+                                    && matrix[row + LPattern[i], col + LPattern[i + 1]] == 'K')
+                                {
+                                    currentKnight++;
+
+                                }
+                            }
+                        }
+
+                        if (currentKnight > maxKnight)
+                        {
+                            maxKnight = currentKnight;
+                            knightRow = row;
+                            knightCol = col;
+                        }
+                    }
+                }
+
+                if (maxKnight == 0)
+                {
+                    break;
+                }
+
+                matrix[knightRow, knightCol] = '0';
+                count++;
+
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static bool IsInside(char[,] matrix, int row, int col)
+        {
+            bool isTrue = row >= 0 && row < matrix.GetLength(0)
+                && col >= 0 && col < matrix.GetLength(1);
+
+            return isTrue;
+        }
+    }
+}
