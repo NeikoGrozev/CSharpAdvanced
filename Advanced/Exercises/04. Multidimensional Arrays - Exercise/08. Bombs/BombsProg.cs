5dd74ee289489de5ec605e15e6f536948a7367c7
Neiko7 <neiko_grozev@abv.bg>
2019-08-14 08:14:51 +0300

    New tasks...

---
+++ Advanced/Exercises/04. Multidimensional Arrays - Exercise/08. Bombs/BombsProg.cs
@@ -0,0 +1,100 @@
+﻿namespace Bombs
+{
+    using System;
+    using System.Linq;
+
+    class BombsProg
+    {
+        static void Main()
+        {
+            int rowsOfMatrix = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rowsOfMatrix, rowsOfMatrix];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] digit = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = digit[col];
+                }
+            }
+
+            string[] bombCell = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int[] neighbourCells = new int[]
+                {
+                    -1, -1,
+                    -1,  0,
+                    -1,  1,
+                     0,  1,
+                     1,  1,
+                     1,  0,
+                     1, -1,
+                     0, -1,
+
+                };
+
+            for (int i = 0; i < bombCell.Length; i++)
+            {
+                int[] currentBomb = bombCell[i]
+                    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int bombRow = currentBomb[0];
+                int bombCol = currentBomb[1];
+                int bombNumber = matrix[bombRow, bombCol];
+
+                if (bombNumber > 0)
+                {
+                    for (int j = 0; j < neighbourCells.Length; j += 2)
+                    {
+                        int currentBombRow = bombRow + neighbourCells[j];
+                        int currentBombCol = bombCol + neighbourCells[j + 1];
+
+                        if (currentBombRow >= 0 && currentBombRow < matrix.GetLength(0)
+                            && currentBombCol >= 0 && currentBombCol < matrix.GetLength(1))
+                        {
+                            if (matrix[currentBombRow, currentBombCol] > 0)
+                            {
+                                int temp = matrix[currentBombRow, currentBombCol];
+                                matrix[currentBombRow, currentBombCol] = temp - bombNumber;
+                            }
+                        }
+                    }
+
+                    matrix[bombRow, bombCol] = 0;
+                }
+            }
+            int activeCell = 0;
+            int sum = 0;
+
+            foreach (var item in matrix)
+            {
+                if (item > 0)
+                {
+                    activeCell++;
+                    sum += item;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {activeCell}");
+            Console.WriteLine($"Sum: {sum}");
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Advanced/Exercises/04. Multidimensional Arrays - Exercise/09. Miner/MinerProg.cs
@@ -0,0 +1,98 @@
+﻿namespace Miner
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class MinerProg
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[rows, rows];
+
+            string[] commands = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> queue = new Queue<string> (commands);
+
+            int allCoals = 0;
+            int currentCoals = 0;
+            int startRow = -1;
+            int startCol = -1;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] symbols = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = symbols[col];
+
+                    if (symbols[col] == 'c')
+                    {
+                        allCoals++;
+                    }
+
+                    if (symbols[col] == 's')
+                    {
+                        startRow = row;
+                        startCol = col;
+                    }
+                }
+            }
+
+            bool isFinish = false;
+
+            for (int i = 0; i < commands.Length; i++)
+            {
+                string command = queue.Dequeue();
+
+                if (command == "left" && startCol - 1 >= 0)
+                {
+                    startCol--;
+                }
+                else if (command == "right" && startCol + 1 < matrix.GetLength(1))
+                {
+                    startCol++;
+                }
+                else if (command == "up" && startRow - 1 >= 0)
+                {
+                    startRow--;
+                }
+                else if (command == "down" && startRow + 1 < matrix.GetLength(0))
+                {
+                    startRow++;
+                }
+
+                if (matrix[startRow, startCol] == 'e')
+                {
+                    Console.WriteLine($"Game over! ({startRow}, {startCol})");
+                    isFinish = true;
+                    break;
+                }
+                else if (matrix[startRow, startCol] == 'c')
+                {
+                    currentCoals++;
+                    matrix[startRow, startCol] = '*';
+
+                    if (currentCoals == allCoals)
+                    {
+                        Console.WriteLine($"You collected all coals! ({startRow}, {startCol})");
+                        isFinish = true;
+                        break;
+                    }
+                }
+            }
+
+            if (queue.Count == 0 && !isFinish && currentCoals < allCoals)
+            {
+                int coalsLeft = allCoals - currentCoals;
+
+                Console.WriteLine($"{coalsLeft} coals left. ({startRow}, {startCol})");
+            }
+        }        
+    }
+}
