f1f4340119b8de09a01f457c1e1c5b3ed84744cd
Neiko7 <neiko_grozev@abv.bg>
2019-08-16 15:31:22 +0300

    New tasks...

---
+++ Advanced/Exercises/05. Sets and Dictionaries Advanced - Lab/04. Cities by Continent and Country/CitiesByContinentAndCountryProg.cs
@@ -0,0 +1,51 @@
+﻿namespace CitiesByContinentAndCountry
+{
+    using System;
+    using System.Collections.Generic;
+
+    class CitiesByContinentAndCountryProg
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> dict = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = tokens[0];
+                string country = tokens[1];
+                string city = tokens[2];
+
+                if (!dict.ContainsKey(continent))
+                {
+                    dict.Add(continent, new Dictionary<string, List<string>>());
+                    dict[continent].Add(country, new List<string>());
+                    dict[continent][country].Add(city);
+                }
+                else
+                {
+                    if (!dict[continent].ContainsKey(country))
+                    {
+                        dict[continent].Add(country, new List<string>());
+                    }
+
+                    dict[continent][country].Add(city);
+                }
+            }
+
+            foreach (var continent in dict)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var item in continent.Value)
+                {
+                    Console.WriteLine($"    {item.Key} -> {string.Join(", ", item.Value)}");
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/Exercises/05. Sets and Dictionaries Advanced - Lab/05. Record Unique Names/RecordUniqueNamesProg.cs
@@ -0,0 +1,27 @@
+﻿namespace RecordUniqueNames
+{
+    using System;
+    using System.Collections.Generic;
+
+    class RecordUniqueNamesProg
+    {
+        static void Main()
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                string name = Console.ReadLine();
+
+                if (!set.Contains(name))
+                {
+                    set.Add(name);
+                }
+            }
+
+            Console.WriteLine($"{string.Join(Environment.NewLine, set)}");
+        }
+    }
+}
