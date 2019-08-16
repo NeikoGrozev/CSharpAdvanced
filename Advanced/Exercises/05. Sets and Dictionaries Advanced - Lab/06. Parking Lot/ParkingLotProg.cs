0aa138de387d79f587ce8843c2b371ba352153d8
Neiko7 <neiko_grozev@abv.bg>
2019-08-16 16:11:14 +0300

    New tasks...

---
+++ Advanced/Exercises/05. Sets and Dictionaries Advanced - Lab/06. Parking Lot/ParkingLotProg.cs
@@ -0,0 +1,53 @@
+﻿namespace ParkingLot
+{
+    using System;
+    using System.Collections.Generic;
+
+    class ParkingLotProg
+    {
+        static void Main()
+        {
+
+            HashSet<string> set = new HashSet<string>();
+
+            while (true)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                string numberOfCar = tokens[1];
+
+                if (command == "IN")
+                {
+                    if (!set.Contains(numberOfCar))
+                    {
+                        set.Add(numberOfCar);
+                    }
+                }
+                else if (command == "OUT")
+                {
+                    if (set.Contains(numberOfCar))
+                    {
+                        set.Remove(numberOfCar);
+                    }
+                }
+            }
+
+            if (set.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                Console.WriteLine($"{string.Join(Environment.NewLine, set)}");
+            }
+        }
+    }
+}
---
+++ Advanced/Exercises/05. Sets and Dictionaries Advanced - Lab/07. SoftUni Party/SoftUniPartyProg.cs
@@ -0,0 +1,64 @@
+﻿namespace SoftUniParty
+{
+    using System;
+    using System.Collections.Generic;
+
+    class SoftUniPartyProg
+    {
+        static void Main()
+        {
+            HashSet<string> vips = new HashSet<string>();
+            HashSet<string> regulars = new HashSet<string>();
+
+            while (true)
+            {
+                string guest = Console.ReadLine();
+
+                if (guest == "PARTY")
+                {
+                    break;
+                }
+                else if (char.IsDigit(guest[0]))
+                {
+                    vips.Add(guest);
+                }
+                else
+                {
+                    regulars.Add(guest);
+                }
+            }
+
+            while (true)
+            {
+                string guest = Console.ReadLine();
+
+                if (guest == "END")
+                {
+                    break;
+                }
+
+                if (vips.Contains(guest))
+                {
+                    vips.Remove(guest);
+                }
+                else if (regulars.Contains(guest))
+                {
+                    regulars.Remove(guest);
+                }
+            }
+
+            int numbersOfGuest = vips.Count + regulars.Count;
+            Console.WriteLine(numbersOfGuest);
+
+            foreach (var item in vips)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            foreach (var item in regulars)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
