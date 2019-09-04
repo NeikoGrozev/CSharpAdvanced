8326f09bc8da05958ad3301b923a5897ab67de06
Neiko7 <neiko_grozev@abv.bg>
2019-09-04 09:24:57 +0300

    New task...

---
+++ Advanced/Exercises/07. Functional Programming - Lab/04. Add VAT/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace AddVAT
+{
+    using System;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(n => n * 1.2)
+                .ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:F2}");
+            }
+
+        }
+    }
+}
---
+++ Advanced/Exercises/07. Functional Programming - Lab/05. Filter By Age/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace FilterByAge
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<KeyValuePair<string, int>> peoples = new List<KeyValuePair<string, int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(", ");
+
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+
+                peoples.Add(new KeyValuePair<string, int>(name, age));
+            }
+
+            string condition = Console.ReadLine();
+            int filterAge = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+
+            peoples = peoples
+                .Where(x => condition == "younger" ? x.Value < filterAge : x.Value >= filterAge)
+                .ToList();
+
+            foreach (var item in peoples)
+            {
+                Print(item.Key, item.Value, format);
+            }          
+        }
+
+        private static void Print(string key, int value, string format)
+        {
+            if (format == "name age")
+            {
+                Console.WriteLine($"{key} - {value}");
+            }
+            else if (format == "name")
+            {
+                Console.WriteLine($"{key}");
+            }
+            else
+            {
+                Console.WriteLine($"{value}");
+            }
+        }
+    }
+}
