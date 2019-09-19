a5935df9158041f511a4cc04e1b439b6e02835ff
Neiko7 <neiko_grozev@abv.bg>
2019-09-19 21:07:44 +0300

    New tasks...

---
+++ Advanced/Exercises/08. Functional Programming - Exercises/06. Reverse And Exclude/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace ReverseAndExclude
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            int divisibleNum = int.Parse(Console.ReadLine());
+
+            Predicate<int> divisible = number => number % divisibleNum == 0;
+            Action<List<int>> print = numbres => Console.WriteLine(string.Join(" ", numbers));
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (divisible(numbers[i]))
+                {
+                    numbers.Remove(numbers[i]);
+                    i--;
+                }
+            }
+
+            numbers.Reverse();
+            print(numbers);
+
+        }
+    }
+}
---
+++ Advanced/Exercises/08. Functional Programming - Exercises/07. Predicate For Names/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace PredicateForNames
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Predicate<string> nameFunction = name
+                => name.Length <= length;
+
+            Action<string> print = name 
+                => Console.WriteLine(name);
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (nameFunction(names[i]))
+                {
+                    print(names[i]);
+                }
+            }
+        }
+    }
+}
