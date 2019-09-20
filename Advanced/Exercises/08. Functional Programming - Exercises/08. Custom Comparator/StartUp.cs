cc650c0883361a8592a3e7487996d2be5be2b6c1
Neiko7 <neiko_grozev@abv.bg>
2019-09-20 15:56:09 +0300

    New task...

---
+++ Advanced/Exercises/08. Functional Programming - Exercises/08. Custom Comparator/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace CustomComparator
+{
+    using System;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            List<int> evenNumbers = new List<int>();
+            List<int> oddNumbers = new List<int>();
+                       
+            Predicate<int> isEven = number 
+                => number % 2 == 0;
+            
+            foreach (var number in numbers)
+            {
+                if (isEven(number))
+                {
+                    evenNumbers.Add(number);
+                }
+                else
+                {
+                    oddNumbers.Add(number);
+                }
+            }
+
+            evenNumbers.Sort();
+            oddNumbers.Sort();
+
+            Console.Write(string.Join(" ", evenNumbers));
+            Console.Write(" " + string.Join(" ", oddNumbers));
+        }
+    }
+}
