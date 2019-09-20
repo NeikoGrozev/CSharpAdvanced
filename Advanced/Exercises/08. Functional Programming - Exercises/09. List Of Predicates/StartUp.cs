fa463d268c6f47d5f07c63170e4603094fabdfa7
Neiko7 <neiko_grozev@abv.bg>
2019-09-20 16:19:33 +0300

    New task...

---
+++ Advanced/Exercises/08. Functional Programming - Exercises/09. List Of Predicates/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace ListOfPredicates
+{
+    using System;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            int endOfRange = int.Parse(Console.ReadLine());
+            int[] divisibleNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            bool isTrue = false;
+
+            Predicate<int> divisibleFunction = number =>
+            {
+                for (int i = 0; i < divisibleNumbers.Length; i++)
+                {
+                    if (number % divisibleNumbers[i] == 0)
+                    {
+                        isTrue = true;
+                    }
+                    else
+                    {
+                        isTrue = false;
+                        break;
+                    }
+                }
+                return isTrue;
+            };
+
+            for (int i = 1; i <= endOfRange; i++)
+            {
+                if (divisibleFunction(i))
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
