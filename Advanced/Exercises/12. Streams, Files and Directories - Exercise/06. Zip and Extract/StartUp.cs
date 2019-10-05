459375daf028ff5eafc3fc99e373e9a048b4d1d0
Neiko7 <neiko_grozev@abv.bg>
2019-10-05 14:59:48 +0300

    New task...

---
+++ Advanced/Exercises/12. Streams, Files and Directories - Exercise/06. Zip and Extract/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO.Compression;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            ZipFile.CreateFromDirectory("../../../Test folder", "../../../file.zip");
+            ZipFile.ExtractToDirectory("../../../file.zip", "../../../");
+        }
+    }
+}
