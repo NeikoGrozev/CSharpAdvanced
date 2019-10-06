96a4fb5314ef1b7c6240f0d258b97198e8a0cd20
Neiko7 <neiko_grozev@abv.bg>
2019-10-06 11:28:26 +0300

    New exercise...

---
+++ Advanced/Exercises/10. Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person person = new Person();
+            person.Name = name;
+            person.Age = age;
+        }
+    }
+}
---
+++ Advanced/Exercises/10. Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Person personOne = new Person("Pesho", 11);
+        }
+    }
+}
