fb8dec3c488274a84c91b272782663ba02dbfc30
Neiko7 <neiko_grozev@abv.bg>
2019-10-14 20:25:52 +0300

    New task...

---
+++ Advanced/Exercises/16. Iterators and Comparators - Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,37 @@
+﻿namespace Froggy
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> froggyStone;
+
+        public Lake(List<int> input)
+        {
+            this.froggyStone = new List<int>(input);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < froggyStone.Count; i += 2)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return this.froggyStone[i];
+                }
+            }
+
+            for (int i = froggyStone.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 == 1)
+                {
+                    yield return this.froggyStone[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+    }
+}
---
+++ Advanced/Exercises/16. Iterators and Comparators - Exercise/04.Froggy/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace Froggy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<int> input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            var stone = new Lake(input);
+
+            Console.WriteLine(string.Join(", ", stone));
+        }
+    }
+}
