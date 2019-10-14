e6ed409fcd8fecf71f6bbab8ff5c88762f54721a
Neiko7 <neiko_grozev@abv.bg>
2019-10-14 19:44:49 +0300

    New task...

---
+++ Advanced/Exercises/16. Iterators and Comparators - Exercise/03.Stack/CustomStack.cs
@@ -0,0 +1,49 @@
+﻿namespace Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> stack;
+
+        private int index;
+
+        public CustomStack()
+        {
+            this.stack = new List<T>();
+            this.index = 0;
+        }
+
+        public void Push(params T[] elements)
+        {
+            this.stack.AddRange(elements.ToList());
+            this.index = this.stack.Count - 1;
+        }
+
+        public void Pop()
+        {
+            if (this.stack.Count == 0)
+            {
+                Console.WriteLine("No elements");
+
+                return;
+            }
+
+            this.stack.RemoveAt(index);
+            index--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.stack.Count - 1; i >= 0; i--)
+            {
+                yield return this.stack[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+    }
+}
---
+++ Advanced/Exercises/16. Iterators and Comparators - Exercise/03.Stack/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace Stack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            var custumStack = new CustomStack<int>();
+
+            while (true)
+            {
+                List<string> input = Console.ReadLine()
+                    .Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                string command = input[0];
+
+                if (command == "END")
+                {
+                    break;
+                }
+                else if (command == "Push")
+                {
+                    input.RemoveAt(0);
+
+                    int[] numbers = input
+                          .Select(int.Parse)
+                          .ToArray();
+
+                    custumStack.Push(numbers);
+                }
+                else if (command == "Pop")
+                {
+                    custumStack.Pop();
+                }
+            }
+
+            foreach (var item in custumStack)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            foreach (var item in custumStack)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
