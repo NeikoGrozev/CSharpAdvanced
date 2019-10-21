5bfad920c33dba6b805475c7273d83b934ab4995
Neiko7 <neiko_grozev@abv.bg>
2019-10-21 09:15:52 +0300

    New exercise...

---
+++ Advanced/Exercises/19. Workshop3 - Basic Algorithms Exercise/01.RecursiveArraySum/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace RecursiveArraySum
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(Sum(array, array.Length));
+        }
+
+        public static int Sum(int[] array, int index)
+        {
+            if (index == 1)
+            {
+                return array[0];
+            }
+
+            return (array[index - 1] + Sum(array, index - 1));         
+        }
+    }
+}
---
+++ Advanced/Exercises/19. Workshop3 - Basic Algorithms Exercise/02.RecursiveFactorial/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace RecursiveFactorial
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(n));
+        }
+
+        public static long Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
