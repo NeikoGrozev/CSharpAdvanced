1d7dc6eadcf9a65184e8425a9c4e4eee628f2a6e
Neiko7 <neiko_grozev@abv.bg>
2019-11-05 14:14:19 +0200

    New task...

---
+++ OOP/Exercises/06. Encapsulation - Exercise/02.AnimalFarm/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace AnimalFarm
+{
+    using System;
+    using Models;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine($"Chicken {chicken.Name} (age {chicken.Age}) can produce {chicken.ProductPerDay} eggs per day.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+    }
+}
