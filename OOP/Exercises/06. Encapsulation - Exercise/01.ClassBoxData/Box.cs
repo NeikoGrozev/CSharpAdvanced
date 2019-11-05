0b64e9e35b167b2520c3aa31b9a992095117c093
Neiko7 <neiko_grozev@abv.bg>
2019-11-05 13:46:40 +0200

    New exercise...

---
+++ OOP/Exercises/06. Encapsulation - Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                ValidateValue(value, nameof(this.Length));
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                ValidateValue(value, nameof(this.Width));
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                ValidateValue(value, nameof(this.Height));
+                this.height = value;
+            }
+        }
+
+        public double GetSurfaceArea()
+        {
+            return 2 * (this.Length * this.Width) + GetLiteralSurfaceArea();
+        }
+
+        public double GetLiteralSurfaceArea()
+        {
+            return 2 * (this.Length * this.Height + this.Width * this.Height);
+        }
+
+        public double GetVolume()
+        {
+            return this.Length * this.Width * this.Height;
+        }
+
+        private void ValidateValue(double value, string parameters)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException($"{parameters} cannot be zero or negative.");
+            }
+        }
+    }
+}
---
+++ OOP/Exercises/06. Encapsulation - Exercise/01.ClassBoxData/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassBoxData
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.GetSurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.GetLiteralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.GetVolume():F2}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
