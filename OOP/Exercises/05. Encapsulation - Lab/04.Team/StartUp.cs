c3a3651d6cf1a0ba29e6e388ffbdb539fdc25708
Neiko7 <neiko_grozev@abv.bg>
2019-11-05 12:45:31 +0200

    New exercise...

---
+++ OOP/Exercises/05. Encapsulation - Lab/04.Team/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace PersonsInfo
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Team team = new Team("SoftUni");
+
+            var lines = int.Parse(Console.ReadLine());
+           
+            for (int i = 0; i < lines; i++)
+            {
+                try
+                {
+                    var cmdArgs = Console.ReadLine().Split();
+                    var person = new Person(cmdArgs[0],
+                                            cmdArgs[1],
+                                            int.Parse(cmdArgs[2]),
+                                            decimal.Parse(cmdArgs[3]));
+
+                    team.AddPlayer(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine(team);
+        }
+    }
+}
---
+++ OOP/Exercises/05. Encapsulation - Lab/04.Team/Team.cs
@@ -0,0 +1,59 @@
+﻿namespace PersonsInfo
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.FirstTeam = new List<Person>();
+            this.ReserveTeam = new List<Person>();
+        }
+
+        public List<Person> FirstTeam
+        {
+            get => this.firstTeam;
+            private set
+            {
+                this.firstTeam = value;
+            }
+        }
+
+        public List<Person> ReserveTeam
+        {
+            get => this.reserveTeam;
+            private set
+            {
+                this.reserveTeam = value;
+            }
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                this.FirstTeam.Add(person);
+            }
+            else
+            {
+                this.ReserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"First team has {this.FirstTeam.Count} players.");
+            sb.AppendLine($"Reserve team has {this.ReserveTeam.Count} players.");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
