b7b902010635819d5323e5f108bddc5b54331194
Neiko7 <neiko_grozev@abv.bg>
2019-11-05 14:48:33 +0200

    Update class Kitten...

--- OOP/Exercises/04. Inheritance - Exercise/06.Animals/Kitten.cs
+++ OOP/Exercises/04. Inheritance - Exercise/06.Animals/Kitten.cs
@@ -2,7 +2,7 @@
 {
     public class Kitten : Cat
     {
-        private const string gender = "Famale";
+        private const string gender = "Female";
         public Kitten(string name, int age)
             : base(name, age, gender)
         {
