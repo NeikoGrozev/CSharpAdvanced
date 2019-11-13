bc76b318ac7d865e0445f3da8b99d964f17e080c
Neiko7 <neiko_grozev@abv.bg>
2019-11-13 11:06:05 +0200

    New exercise...

---
+++ OOP/Exercises/07. Interfaces and Abstraction - Lab/01.Shapes/Circle.cs
@@ -0,0 +1,42 @@
+﻿namespace Shapes
+{
+    using System;
+
+    public class Circle : IDrawable
+    {
+        private const char symbol = '*';
+        private const char midSymbol = ' ';
+
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public void Draw()
+        {
+            double rIn = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+
+            for (double y = this.radius; y >= -this.radius; y--)
+            {
+                for (double x = -this.radius; x < rOut; x+=0.5)
+                {
+                    double point = x * x + y * y;
+
+                    if(point >= rIn * rIn &&  point <= rOut * rOut)
+                    {
+                        Console.Write(symbol);
+                    }
+                    else
+                    {
+                        Console.Write(midSymbol);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ OOP/Exercises/07. Interfaces and Abstraction - Lab/01.Shapes/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿namespace Shapes
+{
+    using System;
+
+    public class Rectangle : IDrawable
+    {
+        private const char symbol = '*';
+        private const char midSymbol = ' ';
+
+        private int width;
+        private int height;
+
+        public Rectangle(int widht, int height)
+        {
+            this.width = widht;
+            this.height = height;
+        }
+
+        public void Draw()
+        {
+            DrawLine(symbol, symbol);
+
+            for (int i = 0; i < this.height - 2; i++)
+            {
+                DrawLine(symbol, midSymbol);
+            }
+
+            DrawLine(symbol, symbol);
+        }
+
+        private void DrawLine(char symbol, char midSymbol)
+        {
+            Console.WriteLine(symbol + new string(midSymbol, this.width - 2) + symbol);
+        }
+    }
+}
