9403620892a2ec0a2e5d7331e58a5c027f193102
Neiko7 <neiko_grozev@abv.bg>
2019-11-14 10:02:34 +0200

    New task...

---
+++ OOP/Exercises/08. Interfaces and Abstraction - Exercise/10.ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, string country, int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+
+        public string Name { get; }
+
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+
+        string IPerson.GetName()
+        {
+            return $"{this.Name}";
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }
+    }
+}
---
+++ OOP/Exercises/08. Interfaces and Abstraction - Exercise/10.ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace ExplicitInterfaces
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if(input == "End")
+                {
+                    break;
+                }
+
+                string[] args = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = args[0];
+                string country = args[1];
+                int age = int.Parse(args[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+
+                IPerson person = citizen;
+                IResident resident = citizen;
+
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
