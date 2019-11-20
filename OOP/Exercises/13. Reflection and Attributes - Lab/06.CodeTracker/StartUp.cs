b5b13f5c7c3a879ca81e245d8abc4c13647918da
Neiko7 <neiko_grozev@abv.bg>
2019-11-20 17:27:28 +0200

    Add new task...

---
+++ OOP/Exercises/13. Reflection and Attributes - Lab/06.CodeTracker/StartUp.cs
@@ -0,0 +1,10 @@
+﻿[Author("Ventsi")]
+public class StartUp
+{
+    [Author("Gosho")]
+    public static void Main()
+    {
+        Tracker tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
+    }
+}
---
+++ OOP/Exercises/13. Reflection and Attributes - Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Linq;
+using System.Reflection;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        Type type = typeof(StartUp);
+        MethodInfo[] methods = type.GetMethods(
+            BindingFlags.Instance |
+            BindingFlags.Static |
+            BindingFlags.Public);
+
+        foreach (var method in methods)
+        {
+            if (method.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+            {
+                var attributes = method.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute attr in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                }
+            }
+        }
+    }
+}
