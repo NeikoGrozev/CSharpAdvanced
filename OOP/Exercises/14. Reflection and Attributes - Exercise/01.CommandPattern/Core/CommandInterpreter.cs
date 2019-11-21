ab547c5b3fbb0257160c367c1b1a206dbd2baaed
Neiko7 <neiko_grozev@abv.bg>
2019-11-21 11:39:51 +0200

    Add new exercise...

---
+++ OOP/Exercises/14. Reflection and Attributes - Exercise/01.CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,40 @@
+﻿namespace CommandPattern.Core
+{
+    using Contracts;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string postfix = "Command";
+        public string Read(string args)
+        {
+            string[] inputCommnad = args
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string command = inputCommnad[0];
+
+            string[] commandArguments = inputCommnad
+                .Skip(1)
+                .ToArray();
+
+            string fullCommand = command + postfix;
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            Type[] allType = assembly
+                .GetTypes();
+
+            Type currentType = allType.FirstOrDefault(x => x.Name == fullCommand);
+
+            Object instance = Activator.CreateInstance(currentType);
+
+            ICommand commandRun = (ICommand)instance;
+
+            string result = commandRun.Execute(commandArguments);
+
+            return result;                
+        }
+    }
+}
