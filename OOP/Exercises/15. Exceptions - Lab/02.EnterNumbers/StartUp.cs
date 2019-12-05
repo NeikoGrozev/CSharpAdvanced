ec9129939f241f29ce8ae27235009d7e121b3204
Neiko7 <neiko_grozev@abv.bg>
2019-12-05 19:22:07 +0200

    Add new tasks...

---
+++ OOP/Exercises/15. Exceptions - Lab/02.EnterNumbers/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace EnterNumbers
+{
+    using System;
+
+    public class StartUp
+    {
+
+
+        public static void Main()
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    RealNumber(start, end);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid number");
+                    i--;
+                }
+            }
+        }
+
+        public static void RealNumber(int start, int end)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < start || number > end)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
---
+++ OOP/Exercises/15. Exceptions - Lab/03.Fixing/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace Fixing
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            try
+            {
+                string[] weekdays = new string[5];
+                weekdays[0] = "Sunday";
+                weekdays[1] = "Monday";
+                weekdays[2] = "Tuesday";
+                weekdays[3] = "Wednesday";
+                weekdays[4] = "Thursday";
+
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(weekdays[i].ToString());
+                } 
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
