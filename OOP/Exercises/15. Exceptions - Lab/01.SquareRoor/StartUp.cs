733d36f0099d899f454e1501763d197998efc358
Neiko7 <neiko_grozev@abv.bg>
2019-12-05 18:45:59 +0200

    Add new exercise...

---
+++ OOP/Exercises/15. Exceptions - Lab/01.SquareRoor/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace SquareRoor
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if(number < 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                double result = Math.Sqrt(number);
+
+                Console.WriteLine(result);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
